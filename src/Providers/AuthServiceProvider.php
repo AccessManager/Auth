70ec666a518a51f4f6f17788c83d37ef2cdd028a
Rahul makhija <rahulmkhj@gmail.com>
2017-06-18 23:31:46 +0530

    initial commit.

---
+++ src/Providers/AuthServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AM3\Auth\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../Views', 'Auth');
+    }
+}
