c5fe7250273ac8294d8f205004b85465a0af10b3
Rahul makhija <rahulmkhj@gmail.com>
2017-08-01 20:25:51 +0530

    added routes file with login routes.

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,1 +1,17 @@
 <?php
+
+Route::group([
+    'namespace'     =>  'AccessManager\Auth\Http\Controllers',
+], function(){
+
+    Route::get('login', [
+        'as'    =>  'login.get',
+        'uses'  =>  'LoginController@getLogin',
+    ]);
+
+    Route::post('login', [
+        'as'    =>  'login.post',
+        'uses'  =>  'LoginController@postLogin',
+    ]);
+    
+});
