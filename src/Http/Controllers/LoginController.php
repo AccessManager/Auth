fcf50e6609b84114ef2e5092720ecce9950be531
Rahul makhija <rahulmkhj@gmail.com>
2017-09-20 21:10:03 +0530

    updated login controller.
    added logout method.
    implemented postLogin method.

--- src/Http/Controllers/LoginController.php
+++ src/Http/Controllers/LoginController.php
@@ -3,17 +3,44 @@
 namespace AccessManager\Auth\Http\Controllers;
 
 
+use AccessManager\Auth\Http\Requests\AdminLoginRequest;
 use AccessManager\Base\Http\Controller\AdminBaseController;
+use Illuminate\Support\MessageBag;
 
+/**
+ * Class LoginController
+ * @package AccessManager\Auth
+ */
 class LoginController extends AdminBaseController
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getLogin()
     {
         return view("Auth::login");
     }
 
-    public function postLogin()
+    /**
+     * @param AdminLoginRequest $request
+     * @return $this|\Illuminate\Http\RedirectResponse
+     */
+    public function postLogin( AdminLoginRequest $request )
     {
-        return redirect('accounts.index');
+        $authenticated = auth()->attempt(
+                        $request->only('username', 'password')
+                    );
+        if( $authenticated )    return redirect()->intended();
+
+        return back()->withErrors(new MessageBag(['status'=>'login failed, try again.']));
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function getLogout()
+    {
+        auth()->logout();
+        return redirect()->intended();
     }
 }
