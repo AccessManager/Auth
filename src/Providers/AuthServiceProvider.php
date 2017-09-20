8c2b396cab9c90a2fa65e779f25f1322b4943150
Rahul makhija <rahulmkhj@gmail.com>
2017-09-20 21:11:48 +0530

    updated AuthoServiceProvider
    registered migration to the service provider.
    registered newly created command ResetAdminPasswordCommand.

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -3,13 +3,29 @@
 namespace AccessManager\Auth\Providers;
 
 
+use AccessManager\Auth\Commands\ResetAdminPasswordCommand;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class AuthServiceProvider
+ * @package AccessManager\Auth
+ */
 class AuthServiceProvider extends ServiceProvider
 {
+    /**
+     * @var array
+     */
+    protected $commands = [
+        ResetAdminPasswordCommand::class,
+    ];
+
+
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/../Views', 'Auth');
+        $this->loadMigrationsFrom( __DIR__ . "/../Database/Migrations");
+
+        $this->commands($this->commands);
     }
 }
